e51dcb3152cdafec1d790eb13391dbca4c9bd0cb
Enamul Hoque <enamulhoque007m@gmail.com>
2025-03-13 03:47:35 +0600

    Tutorial.cs
    Update class

--- Models/Tutorial.cs
+++ Models/Tutorial.cs
@@ -5,9 +5,10 @@ namespace ArticleCRUD.Models
 {
     public class Tutorial
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; } 
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Article> Articles { get; set; }
+        public string Name { get; set; }= string.Empty;
+        public string Description { get; set; }= string.Empty;
+       public List<Article> Articles { get; set; } = new();
     }
 }
