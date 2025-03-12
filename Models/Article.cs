48065c6572e7832ea7671ef1e6a73dbaa98126dc
Enamul Hoque <enamulhoque007m@gmail.com>
2025-03-13 03:45:20 +0600

    Article.cs
    ccc

--- Models/Article.cs
+++ Models/Article.cs
@@ -3,11 +3,12 @@ namespace ArticleCRUD.Models
 {
     public class Article
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ArticleId { get; set; }
         public string ArticleTitle { get; set; }
         public string ArticleContent { get; set; }
 
-        //Setup relationship with Tutorial model/table        
+        [ForeignKey(nameof(Tutorial))]       
         public int TutorialId { get; set; } //Foreign Key
         public Tutorial Tutorial { get; set; } //Reference navigaton property
     }
